463ff20e629c0042bb15c9fb6ac881b68cabf903
Ben Carpenter <carpenter@earthlinginteractive.com>
2016-03-21 15:00:42 -0500

    basic routing

---
+++ index.ios.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View
+} from 'react-native';
+
+var Main = require('./App/Components/Main');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
+class githubNotetaker extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style = {styles.container}
+        initialRoute = {{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
+    );
+  }
+}
+
+
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
