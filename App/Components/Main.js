6574dd4c53d93197759e7341e295602f70b85673
Ben Carpenter <carpenter@earthlinginteractive.com>
2016-03-23 13:36:25 -0500

    state and touch events

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -4,7 +4,10 @@ var {
     View,
     Text,
     Component,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    TouchableHighlight,
+    ActivityIndicatorIOS
     } = React;
 
 const styles = StyleSheet.create({
@@ -52,10 +55,48 @@ const styles = StyleSheet.create({
 });
 
 class Main extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            isLoading: false,
+            error: false
+        }
+    }
+
+    handleChange(event){
+        this.setState({
+            username: event.nativeEvent.text
+        });
+    }
+
+    handleSubmit(){
+        //update our indicatorIOS spinner
+        this.setState({
+            isLoading: true
+        });
+        console.log('Submit', this.state.username);
+
+        //fetch data from github
+        //reroute to the next passing that github info
+    }
+
     render() {
         return (
             <View style = {styles.mainContainer}>
-                <Text>Testing the router</Text>
+                <Text style={styles.title}>Search for a Github User</Text>
+                <TextInput
+                    style={styles.searchInput}
+                    value={this.state.username}
+                    onChange={this.handleChange.bind(this)}
+                />
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={this.handleSubmit.bind(this)}
+                    underlayColor="white"
+                >
+                    <Text style={styles.buttonText}>Search</Text>
+                </TouchableHighlight>
             </View>
         )
     }
