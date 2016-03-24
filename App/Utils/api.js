22bbf4551a539ac083b40334f4cda37d30d18cfb
Ben Carpenter <carpenter@earthlinginteractive.com>
2016-03-24 12:36:24 -0500

    add funcitons to save/get notes with firebase

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -9,6 +9,21 @@ var api = {
         username = username.toLowerCase().trim();
         var url = `https://api.github.com/users/${username}`;
         return fetch(url).then((res) => res.json());
+    },
+
+    getNotes(username){
+        username = username.toLowerCase().trim();
+        var url = `https://ghsave.firebaseio.com/${username}.json`;
+        return fetch(url).then((res) => res.json());
+    },
+
+    addNotes(username, note){
+        username = username.toLowerCase().trim();
+        var url = `https://ghsave.firebaseio.com/${username}.json`;
+        return fetch(url, {
+            method: 'post',
+            body: JSON.stringify(note)
+        }).then((res) => res.json());
     }
 };
 
