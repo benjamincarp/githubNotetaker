c0a750a613d082745bab74ee96324b07176298a3
Ben Carpenter <carpenter@earthlinginteractive.com>
2016-03-28 12:49:20 -0500

    android sort of starting to work

--- index.android.js
+++ index.android.js
@@ -4,48 +4,47 @@
  */
 
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    Navigator,
+    View
 } from 'react-native';
 
-class githubNotetaker extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var Main = require('./App/Components/Main');
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#111111'
+    },
 });
+
+// The Router wrapper
+class githubNotetaker extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Navigator
+                    initialRoute={{name: 'Github Notetaker', component: Main}}
+                    renderScene={(route, navigator) => {
+                      if (route.component){
+                        var props = {navigator: navigator};
+
+                        if (route.passProps) {
+                            for (var prop in route.passProps){
+                                props[prop] = route.passProps[prop];
+                            }
+                        }
+                        return React.createElement(route.component, props);
+                      }
+                    }}
+                />
+            </View>
+        );
+    }
+}
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
